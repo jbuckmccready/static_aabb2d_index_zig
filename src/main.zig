cbd8056d3c80336e4b94fa31a129d54b773a9a04
Buck McCready <jbuckmccready@gmail.com>
2023-08-29 10:48:52 -0700

    Continued WIP
    - Basic query and visitQuery working
    - Compiles and handles floats and ints

--- src/main.zig
+++ src/main.zig
@@ -17,16 +17,36 @@ pub fn AABB(comptime T: type) type {
                 .max_y = max_y,
             };
         }
+
+        pub fn overlaps(self: Self, min_x: T, min_y: T, max_x: T, max_y: T) bool {
+            if (self.max_x < min_x or
+                self.max_y < min_y or
+                self.min_x > max_x or
+                self.min_y > max_y)
+            {
+                return false;
+            }
+
+            return true;
+        }
     };
 }
 
 pub fn StaticAABB2DIndex(comptime T: type) type {
     return struct {
+        /// Node size defined  when the index was built.
         node_size: usize,
+        /// Total number of items in the index.
         num_items: usize,
+        /// Level bounds for traversing the index tree.
         level_bounds: []usize,
+        /// All the AABB boxes (items and tree containers).
         boxes: []AABB(T),
+        /// Indices to map from a `boxes` index back to the original index position the item was
+        /// added. Note there ar more indices than `num_items`, the first `num_items` items in the
+        /// slice are associated with the `boxes`.
         indices: []usize,
+        /// Allocator used to manage the memory of the spatial index.
         allocator: std.mem.Allocator,
 
         const Self = @This();
@@ -40,9 +60,79 @@ pub fn StaticAABB2DIndex(comptime T: type) type {
             return self.boxes[self.boxes.len - 1];
         }
 
-        /// Gets the total count of items that were added to the index during construction.
-        pub fn count(self: Self) usize {
-            return self.num_items;
+        fn collectingVisitor(context: *std.ArrayList(usize), index: usize) error{OutOfMemory}!bool {
+            try context.append(index);
+            return true;
+        }
+        pub fn query(
+            self: Self,
+            min_x: T,
+            min_y: T,
+            max_x: T,
+            max_y: T,
+            allocator: std.mem.Allocator,
+        ) error{OutOfMemory}!std.ArrayList(usize) {
+            var result = std.ArrayList(usize).init(allocator);
+            errdefer result.deinit();
+            var stack = std.ArrayList(usize).init(allocator);
+            defer stack.deinit();
+
+            self.visitQueryWithStack(
+                min_x,
+                min_y,
+                max_x,
+                max_y,
+                &result,
+                collectingVisitor,
+                &stack,
+            ) catch return error.OutOfMemory;
+
+            return result;
+        }
+
+        /// Perform query with a visitor function and using an existing stack for traversal, if the
+        /// function returns true the query continues, otherwise the query stops.
+        pub fn visitQueryWithStack(
+            self: Self,
+            min_x: T,
+            min_y: T,
+            max_x: T,
+            max_y: T,
+            context: anytype,
+            comptime visitorFn: fn (@TypeOf(context), index: usize) anyerror!bool,
+            stack: *std.ArrayList(usize),
+        ) !void {
+            var node_index = self.boxes.len - 1;
+            var level = self.level_bounds.len - 1;
+            stack.clearRetainingCapacity();
+
+            while (true) {
+                const end = @min(node_index + self.node_size, self.level_bounds[level]);
+                for (node_index..end) |pos| {
+                    const aabb = self.boxes[pos];
+                    if (!aabb.overlaps(min_x, min_y, max_x, max_y)) {
+                        // no overlap
+                        continue;
+                    }
+
+                    const index = self.indices[pos];
+                    if (node_index < self.num_items) {
+                        if (!try visitorFn(context, index)) {
+                            return;
+                        }
+                    } else {
+                        try stack.append(index);
+                        try stack.append(level - 1);
+                    }
+                }
+
+                if (stack.items.len > 1) {
+                    level = stack.pop();
+                    node_index = stack.pop();
+                } else {
+                    return;
+                }
+            }
         }
 
         pub fn deinit(self: Self) void {
@@ -74,7 +164,15 @@ pub fn StaticAABB2DIndexBuilder(comptime T: type) type {
 
         const Self = @This();
 
-        pub fn init(allocator: std.mem.Allocator, num_items: usize, node_size: usize) error{OutOfMemory}!Self {
+        pub fn init(allocator: std.mem.Allocator, num_items: usize) error{OutOfMemory}!Self {
+            return try Self.initWithNodeSize(allocator, num_items, 16);
+        }
+
+        pub fn initWithNodeSize(
+            allocator: std.mem.Allocator,
+            num_items: usize,
+            node_size: usize,
+        ) error{OutOfMemory}!Self {
             if (num_items == 0) {
                 // just return early, with no items added
                 return Self{
@@ -159,6 +257,43 @@ pub fn StaticAABB2DIndexBuilder(comptime T: type) type {
             self.pos += 1;
         }
 
+        const typesSupportedMsg = "only int and float types are supported";
+        fn u16ToNumType(input: u16) T {
+            return switch (@typeInfo(T)) {
+                .Float => @floatFromInt(input),
+                .Int => @intCast(input),
+                else => @compileError(typesSupportedMsg),
+            };
+        }
+
+        fn numTypeToU16(num: T) u16 {
+            return switch (@typeInfo(T)) {
+                .Float => @intFromFloat(num),
+                .Int => @intCast(num),
+                else => @compileError(typesSupportedMsg),
+            };
+        }
+
+        fn numTypeDiv(numerator: T, denominator: T) T {
+            return switch (@typeInfo(T)) {
+                .Float => numerator / denominator,
+                .Int => @divTrunc(numerator, denominator),
+                else => @compileError(typesSupportedMsg),
+            };
+        }
+
+        const numTypeMinVal = switch (@typeInfo(T)) {
+            .Float => std.math.floatMin(T),
+            .Int => @as(T, std.math.minInt(T)),
+            else => @compileError(typesSupportedMsg),
+        };
+
+        const numTypeMaxVal = switch (@typeInfo(T)) {
+            .Float => std.math.floatMax(T),
+            .Int => @as(T, std.math.maxInt(T)),
+            else => @compileError(typesSupportedMsg),
+        };
+
         pub fn build(self: *Self) StaticAABB2DIndexBuildError!StaticAABB2DIndex(T) {
             if (self.pos != self.num_items) {
                 return StaticAABB2DIndexBuildError.WrongItemCount;
@@ -192,8 +327,8 @@ pub fn StaticAABB2DIndexBuilder(comptime T: type) type {
             const width = max_x - min_x;
             const height = max_y - min_y;
             // hilbert max input value for x and y
-            const hilbert_max: T = @floatFromInt(std.math.maxInt(u16));
-            const two: T = @floatFromInt(2);
+            const hilbert_max: T = u16ToNumType(std.math.maxInt(u16));
+            const two: T = u16ToNumType(2);
             // mapping the x and y coordinates of the center of the item boxes to values in the range
             // [0 -> n - 1] such that the min of the entire set of bounding boxes maps to 0 and the max
             // of the entire set of bounding boxes maps to n - 1 our 2d space is x: [0 -> n-1] and
@@ -205,11 +340,13 @@ pub fn StaticAABB2DIndexBuilder(comptime T: type) type {
                 const aabb = self.boxes[i];
                 var x: u16 = 0;
                 if (width != @as(T, 0)) {
-                    x = @intFromFloat(hilbert_max * ((aabb.min_x + aabb.max_x) / two - min_x) / width);
+                    const x_mid = numTypeDiv((aabb.min_x + aabb.max_x), two);
+                    x = numTypeToU16(hilbert_max * numTypeDiv(x_mid - min_x, width));
                 }
                 var y: u16 = 0;
                 if (height != @as(T, 0)) {
-                    y = @intFromFloat(hilbert_max * ((aabb.min_y + aabb.max_y) / two - min_y) / height);
+                    const y_mid = numTypeDiv((aabb.min_y + aabb.max_y), two);
+                    y = numTypeToU16(hilbert_max * numTypeDiv((y_mid - min_y), height));
                 }
 
                 hilbert_values[i] = hilbertXYToIndex(x, y);
@@ -232,10 +369,10 @@ pub fn StaticAABB2DIndexBuilder(comptime T: type) type {
                 const end = self.level_bounds[i];
 
                 while (pos < end) {
-                    var node_min_x = std.math.floatMax(T);
-                    var node_min_y = std.math.floatMax(T);
-                    var node_max_x = std.math.floatMin(T);
-                    var node_max_y = std.math.floatMin(T);
+                    var node_min_x = numTypeMaxVal;
+                    var node_min_y = numTypeMaxVal;
+                    var node_max_x = numTypeMinVal;
+                    var node_max_y = numTypeMinVal;
                     const node_index = pos;
 
                     // calculate bounding box for the new node
@@ -373,7 +510,7 @@ fn sort(
         }
 
         while (true) {
-            j = j +% 1;
+            j = j -% 1;
             if (values[j] <= pivot) {
                 break;
             }
@@ -394,7 +531,7 @@ fn swap(comptime T: type, values: []u32, boxes: []AABB(T), indices: []usize, i: 
 }
 
 test "wrong item count build error" {
-    var builder = try StaticAABB2DIndexBuilder(f64).init(std.testing.allocator, 2, 16);
+    var builder = try StaticAABB2DIndexBuilder(f64).initWithNodeSize(std.testing.allocator, 2, 16);
     defer builder.deinit();
     var failed_build = builder.build();
     try std.testing.expectError(StaticAABB2DIndexBuildError.WrongItemCount, failed_build);
@@ -408,7 +545,7 @@ test "wrong item count build error" {
 }
 
 test "bounds null when 0 items" {
-    var builder = try StaticAABB2DIndexBuilder(f64).init(std.testing.allocator, 0, 16);
+    var builder = try StaticAABB2DIndexBuilder(f64).initWithNodeSize(std.testing.allocator, 0, 16);
     defer builder.deinit();
     const spatial_index = try builder.build();
     defer spatial_index.deinit();
@@ -416,7 +553,7 @@ test "bounds null when 0 items" {
 }
 
 test "bounds values when not 0 items" {
-    var builder = try StaticAABB2DIndexBuilder(f64).init(std.testing.allocator, 2, 16);
+    var builder = try StaticAABB2DIndexBuilder(f64).initWithNodeSize(std.testing.allocator, 2, 16);
     defer builder.deinit();
     builder.add(0, 0, 1, 1);
     builder.add(1, 1, 3, 3);
@@ -425,12 +562,92 @@ test "bounds values when not 0 items" {
     try std.testing.expectEqual(AABB(f64).init(0, 0, 3, 3), spatial_index.bounds().?);
 }
 
-test "count" {
-    var builder = try StaticAABB2DIndexBuilder(f64).init(std.testing.allocator, 2, 16);
+test "expected node size" {
+    var builder = try StaticAABB2DIndexBuilder(f64).initWithNodeSize(std.testing.allocator, 2, 16);
     defer builder.deinit();
     builder.add(0, 0, 1, 1);
     builder.add(1, 1, 3, 3);
     const spatial_index = try builder.build();
     defer spatial_index.deinit();
-    try std.testing.expectEqual(@as(usize, 2), spatial_index.count());
+    try std.testing.expectEqual(@as(usize, 16), spatial_index.node_size);
+}
+
+test "0 expected number of items" {
+    var builder = try StaticAABB2DIndexBuilder(f64).initWithNodeSize(std.testing.allocator, 0, 16);
+    defer builder.deinit();
+    const spatial_index = try builder.build();
+    defer spatial_index.deinit();
+    try std.testing.expectEqual(@as(usize, 0), spatial_index.num_items);
+}
+
+test "expected number of items" {
+    var builder = try StaticAABB2DIndexBuilder(f64).initWithNodeSize(std.testing.allocator, 2, 16);
+    defer builder.deinit();
+    builder.add(0, 0, 1, 1);
+    builder.add(1, 1, 3, 3);
+    const spatial_index = try builder.build();
+    defer spatial_index.deinit();
+    try std.testing.expectEqual(@as(usize, 2), spatial_index.num_items);
+}
+
+fn createTestData(comptime T: type) *const [400]T {
+    return &[_]T{
+        8,  62, 11, 66, 57, 17, 57, 19, 76, 26, 79, 29, 36, 56, 38, 56, 92, 77, 96, 80, 87, 70, 90,
+        74, 43, 41, 47, 43, 0,  58, 2,  62, 76, 86, 80, 89, 27, 13, 27, 15, 71, 63, 75, 67, 25, 2,
+        27, 2,  87, 6,  88, 6,  22, 90, 23, 93, 22, 89, 22, 93, 57, 11, 61, 13, 61, 55, 63, 56, 17,
+        85, 21, 87, 33, 43, 37, 43, 6,  1,  7,  3,  80, 87, 80, 87, 23, 50, 26, 52, 58, 89, 58, 89,
+        12, 30, 15, 34, 32, 58, 36, 61, 41, 84, 44, 87, 44, 18, 44, 19, 13, 63, 15, 67, 52, 70, 54,
+        74, 57, 59, 58, 59, 17, 90, 20, 92, 48, 53, 52, 56, 92, 68, 92, 72, 26, 52, 30, 52, 56, 23,
+        57, 26, 88, 48, 88, 48, 66, 13, 67, 15, 7,  82, 8,  86, 46, 68, 50, 68, 37, 33, 38, 36, 6,
+        15, 8,  18, 85, 36, 89, 38, 82, 45, 84, 48, 12, 2,  16, 3,  26, 15, 26, 16, 55, 23, 59, 26,
+        76, 37, 79, 39, 86, 74, 90, 77, 16, 75, 18, 78, 44, 18, 45, 21, 52, 67, 54, 71, 59, 78, 62,
+        78, 24, 5,  24, 8,  64, 80, 64, 83, 66, 55, 70, 55, 0,  17, 2,  19, 15, 71, 18, 74, 87, 57,
+        87, 59, 6,  34, 7,  37, 34, 30, 37, 32, 51, 19, 53, 19, 72, 51, 73, 55, 29, 45, 30, 45, 94,
+        94, 96, 95, 7,  22, 11, 24, 86, 45, 87, 48, 33, 62, 34, 65, 18, 10, 21, 14, 64, 66, 67, 67,
+        64, 25, 65, 28, 27, 4,  31, 6,  84, 4,  85, 5,  48, 80, 50, 81, 1,  61, 3,  61, 71, 89, 74,
+        92, 40, 42, 43, 43, 27, 64, 28, 66, 46, 26, 50, 26, 53, 83, 57, 87, 14, 75, 15, 79, 31, 45,
+        34, 45, 89, 84, 92, 88, 84, 51, 85, 53, 67, 87, 67, 89, 39, 26, 43, 27, 47, 61, 47, 63, 23,
+        49, 25, 53, 12, 3,  14, 5,  16, 50, 19, 53, 63, 80, 64, 84, 22, 63, 22, 64, 26, 66, 29, 66,
+        2,  15, 3,  15, 74, 77, 77, 79, 64, 11, 68, 11, 38, 4,  39, 8,  83, 73, 87, 77, 85, 52, 89,
+        56, 74, 60, 76, 63, 62, 66, 65, 67,
+    };
+}
+
+fn aabbFromTestData(comptime T: type, data: []const T) !std.ArrayList(T) {
+    var result = std.ArrayList(T).init(std.testing.allocator);
+    errdefer result.deinit();
+    var i: usize = 0;
+    while (i < data.len) : (i += 4) {
+        try result.append(AABB(T).init(data[i], data[i + 1], data[i + 2], data[i + 3]));
+    }
+
+    return result;
+}
+
+fn createIndexFromData(comptime T: type, data: []const T) !StaticAABB2DIndex(T) {
+    var builder = try StaticAABB2DIndexBuilder(T).init(std.testing.allocator, data.len / 4);
+    var i: usize = 0;
+    while (i < data.len) : (i += 4) {
+        builder.add(data[i], data[i + 1], data[i + 2], data[i + 3]);
+    }
+
+    return builder.build();
+}
+
+fn createTestIndex(comptime T: type) !StaticAABB2DIndex(T) {
+    return createIndexFromData(T, createTestData(T));
+}
+
+test "basic query" {
+    const i32_index = try createTestIndex(i32);
+    defer i32_index.deinit();
+    var i32_results = try i32_index.query(40, 40, 60, 60, std.testing.allocator);
+    defer i32_results.deinit();
+    std.sort.insertion(usize, i32_results.items, {}, std.sort.asc(usize));
+
+    const f64_index = try createTestIndex(f64);
+    defer f64_index.deinit();
+    var f64_results = try f64_index.query(40, 40, 60, 60, std.testing.allocator);
+    defer f64_results.deinit();
+    std.sort.insertion(usize, f64_results.items, {}, std.sort.asc(usize));
 }
